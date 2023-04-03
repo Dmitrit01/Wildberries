c6dea69d34f2219dae8f3192d065bfd70747e659
Dmitriy <Dima.volkov1998@gmail.com>
2023-04-03 19:24:25 +0300

    feat: infinity try

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 import { swipeSlide } from "./modules/swipeSlide.js";
 
 const sliderContainer = document.querySelector(".slider__container");
+const sliderSlides = sliderContainer.querySelectorAll(".slider__item");
 const sliderLeftBtn = document.querySelector(".slider__left-button");
 const sliderRightBtn = document.querySelector(".slider__right-button");
 const sliderDots = document.querySelector(".slider__dots");
+const clidesCount = sliderSlides.length;
+const slideDelay = 400
 
 let currentSlide = 0;
 let autoInterval;
@@ -14,21 +17,39 @@ const elementsArray = [
   sliderDots,
 ];
 
-sliderRightBtn.addEventListener("click", () => {
+for (const slide of sliderSlides) {
+  slide.remove();
+}
+
+sliderContainer.append(sliderSlides[0]);
+
+sliderRightBtn.addEventListener("click", swipeRight);
+
+function swipeRight() {
+  sliderRightBtn.removeEventListener("click", swipeRight);
   currentSlide += 1;
-  if (currentSlide > sliderContainer.childElementCount - 1) {
+  if (currentSlide === clidesCount) {
     currentSlide = 0;
   }
-  swipeSlide(currentSlide);
-});
+  swipeSlide(currentSlide, sliderSlides, "right", slideDelay);
+  setTimeout(() => {
+    sliderRightBtn.addEventListener("click", swipeRight);
+  }, slideDelay);
+}
 
-sliderLeftBtn.addEventListener("click", () => {
+sliderLeftBtn.addEventListener("click", swipeLeft);
+
+function swipeLeft() {
+  sliderLeftBtn.removeEventListener("click", swipeLeft);
   currentSlide -= 1;
   if (currentSlide < 0) {
-    currentSlide = sliderContainer.childElementCount - 1;
+    currentSlide = clidesCount - 1;
   }
-  swipeSlide(currentSlide);
-});
+  swipeSlide(currentSlide, sliderSlides, "left", slideDelay);
+  setTimeout(() => {
+    sliderLeftBtn.addEventListener("click", swipeLeft);
+  }, slideDelay);
+}
 
 sliderDots.addEventListener("click", (event) => {
   const target = event.target;
@@ -37,29 +58,29 @@ sliderDots.addEventListener("click", (event) => {
   swipeSlide(currentSlide);
 });
 
-sliderContainer.addEventListener("mouseout", () => {
-  autoSlider();
-});
+// sliderContainer.addEventListener("mouseout", () => {
+//   autoSlider();
+// });
 
-sliderContainer.addEventListener("mouseover", () => {
-  clearInterval(autoInterval);
-});
+// sliderContainer.addEventListener("mouseover", () => {
+//   clearInterval(autoInterval);
+// });
 
-sliderAddElementsStop(elementsArray);
+// sliderAddElementsStop(elementsArray);
 
-function sliderAddElementsStop(elements) {
-  for (const element of elements) {
-    element.addEventListener("mouseover", () => {
-      clearInterval(autoInterval);
-    });
-  }
-}
+// function sliderAddElementsStop(elements) {
+//   for (const element of elements) {
+//     element.addEventListener("mouseover", () => {
+//       clearInterval(autoInterval);
+//     });
+//   }
+// }
 
-function autoSlider() {
-  autoInterval = setInterval(() => {
-    currentSlide === 4 ? (currentSlide = 0) : currentSlide++;
-    swipeSlide(currentSlide);
-  }, 3000);
-}
+// function autoSlider() {
+//   autoInterval = setInterval(() => {
+//     currentSlide === 5 ? (currentSlide = 0) : currentSlide++;
+//     swipeSlide(currentSlide);
+//   }, 3000);
+// }
 
-autoSlider();
+// autoSlider();
--- src/js/modules/swipeSlide.js
+++ src/js/modules/swipeSlide.js
@@ -1,7 +1,26 @@
 import { slideDotCheck } from "./slideDotCheck.js";
 
-export function swipeSlide(slide) {
+export function swipeSlide(slide, slides, side, delay) {
   const sliderContainer = document.querySelector(".slider__container");
-  sliderContainer.style.transform = `translateX(-${slide * 100}%)`;
+  if (side === "right") {
+    sliderContainer.append(slides[slide]);
+    sliderContainer.style.transform = `translateX(-100%)`;
+  } else {
+    sliderContainer.style.transition = `0s`;
+    sliderContainer.style.transform = `translateX(-100%)`;
+    sliderContainer.prepend(slides[slide]);
+  }
+  console.log(slide);
+  sliderContainer.style.transition = `${delay / 1000}s ease`;
+  setTimeout(() => {
+    if (slides[slide].previousSibling) {
+      slides[slide].previousSibling.remove();
+    } 
+    if (slides[slide].nextElementSibling) {
+      slides[slide].nextElementSibling.remove();
+    }
+    sliderContainer.style.transition = `0s`;
+    sliderContainer.style.transform = `translateX(0%)`;
+  }, delay);
   slideDotCheck(slide);
 }
