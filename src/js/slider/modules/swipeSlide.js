6981ce4a66a384e6bb1df4373633398f25d49d01
Dmitriy <Dima.volkov1998@gmail.com>
2023-04-04 08:44:39 +0300

    ref: make it module

--- src/js/slider/modules/swipeSlide.js
+++ src/js/slider/modules/swipeSlide.js
@@ -1,7 +1,8 @@
 import { slideDotCheck } from "./slideDotCheck.js";
 
-export function swipeSlide(slide, slides, side, delay) {
+export function swipeSlide(slide, slides, delay, side) {
   const sliderContainer = document.querySelector(".slider__container");
+  let currentSlide;
   if (side === "right") {
     sliderContainer.append(slides[slide]);
     sliderContainer.style.transform = `translateX(-100%)`;
@@ -10,12 +11,11 @@ export function swipeSlide(slide, slides, side, delay) {
     sliderContainer.style.transform = `translateX(-100%)`;
     sliderContainer.prepend(slides[slide]);
   }
-  console.log(slide);
   sliderContainer.style.transition = `${delay / 1000}s ease`;
   setTimeout(() => {
     if (slides[slide].previousSibling) {
       slides[slide].previousSibling.remove();
-    } 
+    }
     if (slides[slide].nextElementSibling) {
       slides[slide].nextElementSibling.remove();
     }
---
+++ src/js/slider/slider.js
@@ -0,0 +1,81 @@
+import { swipeSlide } from "./modules/swipeSlide.js";
+export function slider() {
+  const sliderContainer = document.querySelector(".slider__container");
+  const sliderSlides = sliderContainer.querySelectorAll(".slider__item");
+  const sliderLeftBtn = document.querySelector(".slider__left-button");
+  const sliderRightBtn = document.querySelector(".slider__right-button");
+  const sliderDots = document.querySelector(".slider__dots");
+  const clidesCount = sliderSlides.length;
+  const slideDelay = 400;
+
+  let currentSlide = 0;
+  let autoInterval;
+  const elementsArray = [
+    sliderContainer,
+    sliderLeftBtn,
+    sliderRightBtn,
+    sliderDots,
+  ];
+
+  for (const slide of sliderSlides) {
+    slide.remove();
+  }
+
+  sliderContainer.append(sliderSlides[0]);
+
+  sliderRightBtn.addEventListener("click", swipeRight);
+
+  function swipeRight() {
+    sliderRightBtn.removeEventListener("click", swipeRight);
+    currentSlide + 1 === clidesCount ? (currentSlide = 0) : currentSlide++;
+    swipeSlide(currentSlide, sliderSlides, slideDelay, "right");
+    setTimeout(() => {
+      sliderRightBtn.addEventListener("click", swipeRight);
+    }, slideDelay);
+  }
+
+  sliderLeftBtn.addEventListener("click", swipeLeft);
+
+  function swipeLeft() {
+    sliderLeftBtn.removeEventListener("click", swipeLeft);
+    currentSlide - 1 < 0 ? (currentSlide = clidesCount - 1) : currentSlide--;
+    swipeSlide(currentSlide, sliderSlides, slideDelay, "left");
+    setTimeout(() => {
+      sliderLeftBtn.addEventListener("click", swipeLeft);
+    }, slideDelay);
+  }
+
+  sliderDots.addEventListener("click", (event) => {
+    const target = event.target;
+    if (!target.classList.contains("slider__dot")) return;
+    currentSlide = target.dataset.dotnum - 1;
+    swipeSlide(currentSlide, sliderSlides, slideDelay, "right");
+  });
+
+  sliderContainer.addEventListener("mouseout", () => {
+    autoSlider();
+  });
+
+  sliderContainer.addEventListener("mouseover", () => {
+    clearInterval(autoInterval);
+  });
+
+  sliderAddElementsStop(elementsArray);
+
+  function sliderAddElementsStop(elements) {
+    for (const element of elements) {
+      element.addEventListener("mouseover", () => {
+        clearInterval(autoInterval);
+      });
+    }
+  }
+
+  function autoSlider() {
+    autoInterval = setInterval(() => {
+      currentSlide + 1 === clidesCount ? (currentSlide = 0) : currentSlide++;
+      swipeSlide(currentSlide, sliderSlides, "right", slideDelay);
+    }, 3000);
+  }
+
+  autoSlider();
+}
